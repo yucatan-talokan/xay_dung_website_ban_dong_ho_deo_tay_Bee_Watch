9f77b58bdb76056d4fecb1d5ab189a6542935101
yucatan-talokan <domanhcuong27062003@gmail.com>
2023-10-15 16:25:40 +0700

    create package

---
+++ src/main/java/com/example/website_ban_dong_ho_deo_tay_bee_watch/WebsiteBanDongHoDeoTayBeeWatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.website_ban_dong_ho_deo_tay_bee_watch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteBanDongHoDeoTayBeeWatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteBanDongHoDeoTayBeeWatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/website_ban_dong_ho_deo_tay_bee_watch/WebsiteBanDongHoDeoTayBeeWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.website_ban_dong_ho_deo_tay_bee_watch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteBanDongHoDeoTayBeeWatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
