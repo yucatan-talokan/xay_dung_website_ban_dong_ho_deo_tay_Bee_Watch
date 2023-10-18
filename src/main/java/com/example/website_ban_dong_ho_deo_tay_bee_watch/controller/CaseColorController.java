1e5ba80a1370819c6220856d25b273af00f80b69
Datqph20571 <datpqph20571@fpt.edu.vn>
2023-10-18 18:50:18 +0700

    Change shape

---
+++ src/main/java/com/example/website_ban_dong_ho_deo_tay_bee_watch/controller/CaseColorController.java
@@ -0,0 +1,48 @@
+package com.example.website_ban_dong_ho_deo_tay_bee_watch.controller;
+
+import com.example.website_ban_dong_ho_deo_tay_bee_watch.entity.CaseColor;
+import com.example.website_ban_dong_ho_deo_tay_bee_watch.service.CaseColorService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.UUID;
+@Controller
+@RequestMapping("/shellColor")
+public class CaseColorController {
+    @Autowired
+    CaseColorService caseColorService;
+    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
+    public ResponseEntity<List<CaseColor>> listAll() {
+        List<CaseColor> list = caseColorService.getAll();
+        if (list.isEmpty()) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<CaseColor>>(list, HttpStatus.OK);
+    }
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public CaseColor save(@Valid @RequestBody CaseColor caseColor) {
+        return caseColorService.add(caseColor);
+    }
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<CaseColor> deleteContact(@PathVariable(value = "id") UUID id) {
+        caseColorService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<CaseColor> updateStrapById(@PathVariable UUID id, @RequestBody CaseColor caseColor) {
+        CaseColor updatedCaseColor = caseColorService.update(id, caseColor);
+        if (updatedCaseColor != null) {
+            return ResponseEntity.ok(updatedCaseColor);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ src/main/java/com/example/website_ban_dong_ho_deo_tay_bee_watch/controller/GlassMaterialController.java
@@ -0,0 +1,50 @@
+package com.example.website_ban_dong_ho_deo_tay_bee_watch.controller;
+
+import com.example.website_ban_dong_ho_deo_tay_bee_watch.entity.GlassMaterial;
+import com.example.website_ban_dong_ho_deo_tay_bee_watch.entity.Strap;
+import com.example.website_ban_dong_ho_deo_tay_bee_watch.service.GlassMaterialService;
+import com.example.website_ban_dong_ho_deo_tay_bee_watch.service.imp.StrapServiceIpml;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.UUID;
+@Controller
+@RequestMapping("/glassMaterial")
+public class GlassMaterialController {
+    @Autowired
+    GlassMaterialService glassMaterialService;
+    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
+    public ResponseEntity<List<GlassMaterial>> listAll() {
+        List<GlassMaterial> list = glassMaterialService.getAll();
+        if (list.isEmpty()) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<GlassMaterial>>(list, HttpStatus.OK);
+    }
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public GlassMaterial save(@Valid @RequestBody GlassMaterial glassMaterial) {
+        return glassMaterialService.add(glassMaterial);
+    }
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<GlassMaterial> deleteContact(@PathVariable(value = "id") UUID id) {
+        glassMaterialService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<GlassMaterial> updateStrapById(@PathVariable UUID id, @RequestBody GlassMaterial glassMaterial) {
+        GlassMaterial updatedGlass = glassMaterialService.update(id, glassMaterial);
+        if (updatedGlass != null) {
+            return ResponseEntity.ok(updatedGlass);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
