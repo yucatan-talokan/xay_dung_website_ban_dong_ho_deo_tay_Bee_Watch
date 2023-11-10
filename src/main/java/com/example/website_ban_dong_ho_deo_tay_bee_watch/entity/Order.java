a9c828c245c47325b6da41da25a829a67f2365aa
yucatan-talokan <domanhcuong27062003@gmail.com>
2023-11-10 21:00:50 +0700

    create class Order, Orderdetail

--- src/main/java/com/example/website_ban_dong_ho_deo_tay_bee_watch/entity/Order.java
+++ src/main/java/com/example/website_ban_dong_ho_deo_tay_bee_watch/entity/Order.java
@@ -1,4 +1,62 @@
 package com.example.website_ban_dong_ho_deo_tay_bee_watch.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column
+    private UUID id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "total_money")
+    private BigDecimal totalMoney;
+
+    @Column(name = "total_payment")
+    private BigDecimal totalPayment;
+
+    @Column(name = "date_payment")
+    private Date datePayment;
+
+    @Column(name = "shipping_price")
+    private BigDecimal shippingPrice;
+
+    @Column(name = "shipping_date")
+    private Date shippingDate;
+
+    @Column(name = "received_date")
+    private Date receivedDate;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    @Column(name = "create_date")
+    private Date createdDate;
+
+    @Column(name = "update_date")
+    private Date updatedDate;
+
+    @Column(name = "status")
+    private Boolean status;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+
 }
--- src/main/java/com/example/website_ban_dong_ho_deo_tay_bee_watch/entity/OrderDetail.java
+++ src/main/java/com/example/website_ban_dong_ho_deo_tay_bee_watch/entity/OrderDetail.java
@@ -1,4 +1,50 @@
 package com.example.website_ban_dong_ho_deo_tay_bee_watch.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "orderdetails")
 public class OrderDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column
+    private UUID id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_order",referencedColumnName = "id")
+    private Order order;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_watch_detail",referencedColumnName = "id")
+    private WatchDetail watchDetail;
+
+    @Column(name = "quantity")
+    private BigInteger quantity;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @Column(name = "update_date")
+    private Date updatedDate;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
 }
